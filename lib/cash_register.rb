e2c466096115bae6841fc872b5a254f16aaa06c2
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-02-11 03:55:08 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,16 @@
 
+class CashRegister
+  attr_accessor :discount, :total
+
+  def initialize(discount = 0)
+    @total=0
+    @discount = discount
+  end
+
+  def add_item(item, item_price, item_quantity = 1)
+    @total += item_price*item_quantity*(1-@discount/100)
+  end
+
+  def apply_discount
+    self.total *=(1-(@discount.to_f/100))
+end
